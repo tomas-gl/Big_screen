4d47c575bbeb0d89896aeaaa093fd95c8b1cdbc9
tgavrel <tomas.gavrel@hotmail.fr>
2022-09-14 15:14:26 +0200

    Installed Quasar library :package:

--- admin/src/main.js
+++ admin/src/main.js
@@ -1,17 +1,4 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-import PrimeVue from 'primevue/config';
-import ButtonComponent from 'primevue/button';
-import InputText from 'primevue/inputtext';
-
-import 'primevue/resources/themes/bootstrap4-dark-blue/theme.css'
-
-const app = createApp(App);
-
-app.use(PrimeVue);
-
-app.component('ButtonComponent', ButtonComponent);
-app.component('InputText', InputText);
-
-app.mount('#app')
+createApp(App).mount('#app')
