a43e8313018fd62d2a13c9ac18d1514b798901b6
Yassine Reguigui <yassine.reguigui.edu@groupe-gema.com>
2022-09-14 09:53:56 +0200

    Added assets, router, views and components

---
+++ admin/src/components/HeaderUser.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <img :src="logo" class="logo">
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+    return {
+      logo: require("../assets/logo_bigscreen.png")
+    }
+  }
+}
+</script>
+
+<style>
+    img.logo {
+        position: sticky;
+        width: 30%;
+        margin: auto;
+        margin-top: 1%;
+    }
+</style>
