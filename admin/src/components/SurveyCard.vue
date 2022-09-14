02c271cb73e2ec71d639e1f97b6ff48d57dc5d21
tgavrel <tomas.gavrel@hotmail.fr>
2022-09-14 20:07:52 +0200

    Updated answers array :wrench:

--- admin/src/components/SurveyCard.vue
+++ admin/src/components/SurveyCard.vue
@@ -2,25 +2,51 @@
   <el-card class="box-card">
     <h2>Question {{ numQuestion }}/20</h2>
     <p>{{corpsQuestion}}</p>
+    <div v-if="typeQuestion == 'A'">
+      <el-input
+        @change="getAnswer(numQuestion)"
+        v-model="answer"
+        type="textarea"
+        placeholder="Please text"
+      />
+    </div>
+    <div v-else-if="typeQuestion == 'B'">
+      <el-input
+        @change="getAnswer(numQuestion)"
+        v-model="answer"
+        type="email"
+        placeholder="Please email"
+      />
+    </div>
   </el-card>
 </template>
 
 <script>
 export default {
-    props: ['numQuestion', 'corpsQuestion']
+    props: ['numQuestion', 'corpsQuestion', 'typeQuestion'],
+    emits: ["getAnswer"],
+    data() {
+      return {
+        answer: '',
+      }
+    },
+    methods:{
+      getAnswer(value){
+        this.$emit('answerToAnswers', {'questionId' : value, 'answer' : this.answer})
+      }
+    }
 }
 </script>
 
 <style>
     h2 {
-    text-align: left;
-    margin-left: 1%;
+      text-align: left;
     }
     .box-card {
-    box-shadow: 0 15px 30px 1px grey;
-	background: rgba(255, 255, 255, 0.90);
-	border-radius: 5px;
-    color: black;
-	margin: 5em auto;
+      box-shadow: 0 15px 30px 1px grey;
+      background: rgba(255, 255, 255, 0.90);
+      border-radius: 5px;
+      color: black;
+      margin: 5em auto;
     }
 </style>
--- admin/src/views/user/SurveyPage.vue
+++ admin/src/views/user/SurveyPage.vue
@@ -1,11 +1,16 @@
 <template>
     <div>
-        <HeaderUser />
+      <HeaderUser />
+      <form @submit.prevent="saveSurvey">
         <el-row>
           <el-col :span="16" :offset="4" v-for="question in questions" :key="question.numQuestion">
-            <SurveyCard :numQuestion="question.numQuestion" :corpsQuestion="question.corpsQuestion"/>
+            <SurveyCard :numQuestion="question.numQuestion" :corpsQuestion="question.corpsQuestion" :typeQuestion="question.typeQuestion" v-on:answerToAnswers="getAnswers"/>
+          </el-col>
+          <el-col>
+            <el-button type="submit" v-on:click="saveSurvey()">Primary</el-button>
           </el-col>
         </el-row>
+      </form>
     </div>
 </template>
 
@@ -25,17 +30,15 @@ export default {
               numQuestion: 1,
               corpsQuestion: 'Corps 1',
               typeQuestion: 'A',
-              seededAnswers: ['a', 'b', 'c']
+              seededAnswers: ['a', 'b', 'c'],
             },
             {
               numQuestion: 2,
-              corpsQuestion: 'Corps 2'
+              corpsQuestion: 'Corps 2',
+              typeQuestion: 'B',
             },
-            {
-              numQuestion: 3,
-              corpsQuestion: 'Corps 3'
-            }
           ],
+        answers: [],
       }
     },
     methods: {
@@ -43,8 +46,27 @@ export default {
         let url = 'http://127.0.0.1:8000/api/getQuestionsSurvey'
         await axios.get(url).then(response =>{
           console.log(response);
+        for (let i= 1 ; i < 21 ; i++) {
+          this.answers.push({
+            'questionId': i,
+            'answer': ''
+          });
+        }
+        console.log(this.answers);
         }).catch(error => console.log(error))
-      }
+      },
+      getAnswers(value){
+        console.log(value);
+        this.answers.forEach(el => {
+          if(el['questionId'] == value['questionId']){
+            el['answer'] = value['answer'];
+          }
+        });
+        console.log(this.answers);
+      },
+      // saveSurvey(){
+      //     console.log(this.answers);
+      // },
     },
     mounted() {
        this.getQuestions();
