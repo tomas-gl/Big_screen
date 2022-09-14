26840d387c90a51f204b12c9f9b874b85641e174
tgavrel <tomas.gavrel@hotmail.fr>
2022-09-14 15:34:30 +0200

    Installed element-plus :package:

--- admin/src/main.js
+++ admin/src/main.js
@@ -1,4 +1,9 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import ElementPlus from 'element-plus'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+app.use(ElementPlus, { size: 'small', zIndex: 3000 })
+
+
+app.mount('#app')
