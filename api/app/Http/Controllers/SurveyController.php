c383a4139a299d0abf8e64f3c6454751804930aa
tgavrel <tomas.gavrel@hotmail.fr>
2022-09-15 18:39:16 +0200

    Updated migration tables names :wrench:

--- api/app/Http/Controllers/SurveyController.php
+++ api/app/Http/Controllers/SurveyController.php
@@ -6,6 +6,8 @@ use Illuminate\Http\Request;
 use App\Models\Question;
 use App\Models\Survey;
 use App\Models\Answer;
+use App\Models\AnswerUser;
+use App\Models\AnswerQuestion;
 
 class SurveyController extends Controller
 {
@@ -15,7 +17,7 @@ class SurveyController extends Controller
     // Type:
     // Parametres ou champs (data):
     // Role:
-    public function getQuestionsSurvey()
+    public function getQuestionsSurvey() 
     {
         $survey = Survey::all()->first();
         $questions = Question::where('survey_id', $survey->id)->get();
@@ -30,20 +32,58 @@ class SurveyController extends Controller
     // Role:
     public function saveQuestionsSurvey(Request $request)
     {
+        $answerUser = new AnswerUser();
+        $answerUser->token = "test_token";
+        $answerUser->save();
+
         foreach($request->answers as $one)
         {
             $one = json_decode($one);
             $answer = new Answer();
+            $answer->$answer_user_id = $answerUser->id;
             $answer->answer = $one->answer;
             // $answer->question_id = $one->answer;
             $answer->save();
             // return response()->json($one);
         }
-        // return response()->json($arr);
+        // return response()->json($request->test);
         return response()->json([
             'message' => 'Nouveau sondage utilisateur créé !',
             'code' => 200
         ]);
     }
+    
+
+
+
+
+
+    // public function saveQuestionsSurvey($request)
+    // {
+    //     // $answerUser = new AnswerUser();
+    //     // $answerUser->token = "test_token";
+    //     // $answerUser->save();
+
+    //     foreach($request->answers as $one)
+    //     {
+    //         $one = json_decode($one);
+
+    //         // $answer = new Answer();
+    //         // // $answer->$answer_user_id = $answerUser->id;
+    //         // $answer->answer = $one->answer;
+    //         // $answer->save();
+            
+    //         // $answerQuestion = new AnswerQuestion();
+    //         // $answerQuestion->answer_id = $answer->id;
+    //         // $answerQuestion->question_id = $one->questionId;
+    //         // $answerQuestion->save();
+    //     }
+        
+    //     return response()->json($request->all());
+    //     // return response()->json([
+    //     //     'message' => 'Nouveau sondage utilisateur créé !',
+    //     //     'code' => 200
+    //     // ]);
+    // }
 
 }
--- api/database/migrations/2022_09_13_09000_create_answer_users_table.php
+++ api/database/migrations/2022_09_13_09000_create_answer_users_table.php
@@ -13,10 +13,9 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('answer_question', function (Blueprint $table) {
+        Schema::create('answer_users', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('answer_id')->references('id')->on('answers')->onUpdate('cascade')->onDelete('cascade');
-            $table->foreignId('question_id')->references('id')->on('questions')->onUpdate('cascade')->onDelete('cascade');
+            $table->string('token', 255)->nullable();
             $table->timestamps();
         });
     }
@@ -28,6 +27,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('answer_question');
+        Schema::dropIfExists('answer_users');
     }
 };
