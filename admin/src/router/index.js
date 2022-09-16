fdc636d701958c59a1b5c68c2266b3c1057bc4cd
Yassine Reguigui <yassine.reguigui.edu@groupe-gema.com>
2022-09-16 12:21:06 +0200

    Changed routes

--- admin/src/router/index.js
+++ admin/src/router/index.js
@@ -1,4 +1,9 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import AnswersAdmin from '../views/admin/AnswersAdmin.vue'
+import HomeAdmin from '../views/admin/HomeAdmin.vue'
+import LoginAdmin from '../views/admin/LoginAdmin.vue'
+import SurveyAdmin from '../views/admin/SurveyAdmin.vue'
+import SurveyPage from '../views/user/SurveyPage.vue'
 
 const routes = [
     {
